c8b0fad8a5d6253b368c5c1b464e73371ae0f289
unknown <kaitoano0@gmail.com>
2019-06-23 22:09:23 +0530

    first commit

---
+++ Assets/Scripts/Player/Movement/movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 10f;
+    private Rigidbody playerRigidBody;
+    private bool stop = false;
+    private bool isMoving = false;
+    private bool grounded = false;
+    private float xInput,yInput;
+    private float jumpSpeed = 5f;
+    void Start()
+    {
+        playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CharacterController controller = GetComponent<CharacterController>();
+
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        // Move
+        Vector3 tempVect = new Vector3(h, 0, v);
+        tempVect = tempVect.normalized * moveSpeed * Time.deltaTime;
+        controller.SimpleMove(tempVect);
+
+    }
+    private void playerJump()
+    {
+        //need to redo
+    }
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Ground"){
+            grounded = true;
+        }
+    }
+}
