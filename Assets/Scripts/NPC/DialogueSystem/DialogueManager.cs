9063d39ce9c5d5d1e7e713e6555e73cc3828341d
vidura185 <huwh3c@gmail.com>
2019-06-24 20:26:17 +0530

    basic-NPC-dialogue-system
    Implemented NPC interaction and basic dialogue system.

---
+++ Assets/Scripts/NPC/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    //if the dialgue UI is being shown
+    public bool isActive;
+    private Queue<string> sentences;
+
+    [HideInInspector]
+    public Canvas canvas;
+    public Text dialogueBoxText;
+    public Image npcImage;
+    public Text npcDescriptionText;
+    public static DialogueManager Instance {
+        get; private set; }
+    private void Start()
+    {
+        sentences = new Queue<string>();
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.enabled = false;
+    }
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void StartDialogue(Dialogue dialogue, NPCDetails npcDetails)
+    {
+        Debug.Log("Started Conversation with" + dialogue.name);
+        canvas.enabled = true;
+        sentences.Clear();
+
+        npcImage.sprite = npcDetails.image;
+        npcDescriptionText.text = npcDetails.npcDescription;
+
+
+        //get all the sentences from the dialogue
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        DisplayNextSentence();
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string nextSentence = sentences.Dequeue();
+        Debug.Log(nextSentence);
+        dialogueBoxText.text = nextSentence;
+    }
+
+    void EndDialogue()
+    {
+        canvas.enabled = false;
+    }
+}
---
+++ Assets/Scripts/NPC/DialogueSystem/NPCDialogueTrigger.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCDialogueTrigger : MonoBehaviour
+{
+    public NPCDetails npcDetails;
+    public Dialogue dialogue;
+
+    public Canvas canvas;
+    //if dialogue is active disable trigger
+    public bool isActive;
+    public bool playerInRange;
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            DialogueManager.Instance.StartDialogue(dialogue, npcDetails);
+            isActive = true;
+        }
+        if (isActive && Input.GetKeyDown(KeyCode.Space))
+        {
+            DialogueManager.Instance.DisplayNextSentence();
+        }
+    }
+    private void Start()
+    {
+        canvas = GetComponentInChildren<Canvas>();
+        canvas.enabled = false;
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (true || other.gameObject.layer == 10)
+        {
+            playerInRange = true;
+            canvas.enabled = true;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (true|| other.gameObject.layer == 10)
+        {
+            playerInRange = false;
+            canvas.enabled = false;
+        }
+    }
+}
+public enum InteractionType
+{
+    DIALOGUE,
+    QUEST
+}
