f98e9f08972240b324f732c608152f406087b4cd
Byungcharn Lee <lee@calcey.com>
2020-02-10 18:17:33 +0530

    Load Battle Scene on Monster E

---
+++ Assets/Scripts/Monster/BattleLoad.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Collider))]
+public class BattleLoad : MonoBehaviour
+{
+    public GameObject prompt;
+    public bool inRange;
+
+    private void Start()
+    {
+        prompt.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            inRange = true;
+            prompt.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == 10)
+        {
+            inRange = false;
+            prompt.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange)
+        {
+            SceneManager.LoadScene("BattleScene");            
+        }
+    }
+}
+
